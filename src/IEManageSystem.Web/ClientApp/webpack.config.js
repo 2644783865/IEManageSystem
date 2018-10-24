92140bdd4d334708a9fa75b17d626de015c14ba1
chenql <1373611035@qq.com>
2018-10-24 17:50:28 +0800

    reactjs修改

--- src/IEManageSystem.Web/ClientApp/webpack.config.js
+++ src/IEManageSystem.Web/ClientApp/webpack.config.js
@@ -55,6 +55,30 @@ module.exports = {
                     }
                 },
                 exclude: /node_modules/
+            },
+            {
+                test:/\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
+                loaders: [
+                    // 小于10KB的图片会自动转成dataUrl
+                    'url?limit=10240&name=img/[hash:8].[name].[ext]',
+                    'image?{bypassOnDebug:true, progressive:true,optimizationLevel:3,pngquant:{quality:"65-80",speed:4}}'
+                ]
+            },
+            {
+                test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
+                loader: 'url?limit=10000&name=fonts/[hash:8].[name].[ext]'
+            },
+            {
+                test: /\.(tpl|ejs)$/, 
+                loader: 'ejs'
+            },
+            {
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader'
+            },
+            { 
+                test: /\.scss$/, 
+                loader: 'style!css!sass'
             }
         ]
     },
