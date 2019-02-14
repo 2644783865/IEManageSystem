ec354b5446ca949931aff3a1743eae901ddb542a
陈乔龙 <1373611035@qq.com>
2019-02-14 23:02:25 +0800

    nodejs包修改

--- src/IEManageSystem.Web/ClientApp/webpack.common.js
+++ src/IEManageSystem.Web/ClientApp/webpack.common.js
@@ -65,7 +65,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['es2015', 'react'],
+                        presets: ['es2017', 'react'],
                     }
                 },
                 exclude: /node_modules/
